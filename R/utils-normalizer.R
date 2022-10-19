b6b4799329e1fcf1f0dc5a9c67a7d8cb262cc0fd
Sciurus365 <jingmeng.cui@outlook.com>
2022-10-19 15:02:48 +0200

    22.10.19.1
    Add/change some default values; add built-in normalization

---
+++ R/utils-normalizer.R
@@ -0,0 +1,57 @@
+#' Normalizer for the input data
+#'
+#' This is an adapted version of [SparseVFC::norm_vecs()] with `na.rm = TRUE`.
+#' @param x The matrix to be normalized. Each row of x represent a vector.
+#' @noRd
+normalize_vecs <- function(x) {
+	x_noNA <- stats::na.omit(x)
+	n_noNA <- nrow(x_noNA)
+	xmean <- colMeans(x_noNA)
+	x_noNA_centered <- x_noNA - matrix(rep(xmean, n_noNA), nrow = n_noNA, byrow = TRUE)
+	xscale <- sqrt(sum(x_noNA_centered^2)/n_noNA)
+	x_noNA_normalized <- x_noNA_centered / xscale
+
+	x <- (x - matrix(rep(xmean, nrow(x)), nrow = nrow(x), byrow = TRUE))/xscale
+
+	return(
+		structure(
+			x,
+			x_noNA = structure(
+				x_noNA_normalized,
+				mean = xmean,
+				scale = xscale
+			),
+			mean = xmean,
+			scale = xscale
+		)
+	)
+}
+
+# several helpers
+normalize_v <- function(v, ref) {
+	(v-attr(ref, "mean"))/attr(ref, "scale")
+}
+
+normalize_x <- function(x, ref) {
+	(x-attr(ref, "mean")[1])/attr(ref, "scale")
+}
+
+normalize_y <- function(y, ref) {
+	(y-attr(ref, "mean")[2])/attr(ref, "scale")
+}
+
+denormalize_v <- function(v, ref) {
+	v*attr(ref, "scale")+attr(ref, "mean")
+}
+
+scale_up <- function(v, ref) {
+	v*attr(ref, "scale")
+}
+
+denormalize_x <- function(x, ref) {
+	x*attr(ref, "scale")+attr(ref, "mean")[1]
+}
+
+denormalize_y <- function(y, ref) {
+	y*attr(ref, "scale")+attr(ref, "mean")[2]
+}
--- R/vectorfield_landscape.R
+++ R/vectorfield_landscape.R
@@ -1,13 +1,13 @@
 #' Estimate a 2D vector field
 #'
-#' Estimate a 2D vector field from intensive longitudinal data. Two methods can be used: Sparse Vector Field Consensus (SparseVFC, using [SparseVFC::SparseVFC()]), or Multivariate Vector Field Kernel Estimator (MVKE, using [MVKE()]). To make sure the default parameter settings are close to the optimal, it is ***highly recommended to normalize the input data with [norm_vecs()]*** before putting them into this function.
+#' Estimate a 2D vector field from intensive longitudinal data. Two methods can be used: Sparse Vector Field Consensus (SparseVFC, using [SparseVFC::SparseVFC()]), or Multivariate Vector Field Kernel Estimator (MVKE, using [MVKE()]). Note that the input data are automatically normalized before being sent to the estimation engines to make sure the default parameter settings are close to the optimal. Therefore, you do not need to scale up or down the parameters of [SparseVFC::SparseVFC()] or [MVKE()].
 #'
 #' @param d The data set used for estimating the vector field.
 #' Should be a data frame or a matrix.
 #' @param x,y Characters to indicate the name of the two variables.
 #' @param x_start,x_end,x_by,y_start,y_end,y_by Specify the sample grid for the output: the starting, ending, and increment of the sequences for both axes.
-#' @param vector_position One of "start", "middle", or "end". If "start",
-#' @param na_action One of "omit_data_points" or "omit_vectors". If using "omit_data_points", then the vectors will be retained even if there are `NA` points between them. If using "omit_vectors", then the vectors will be omitted if either of its points is `NA`.
+#' @param vector_position Only useful if `method == "VFC"`. One of "start", "middle", or "end", representing the position of the vectors. If "start", for example, the starting point of a vector is regarded as the position of the vector.
+#' @param na_action One of "omit_data_points" or "omit_vectors". If using "omit_data_points", then only the `NA` points are omitted, and the points before and after an `NA` will form a vector. If using "omit_vectors", then the vectors will be omitted if either of its points is `NA`.
 #' @param method One of "VFC" or "MVKE".
 #' @param ... Other parameters to be passed to [SparseVFC::SparseVFC()] or [MVKE()].
 #'
@@ -15,14 +15,20 @@
 #' @seealso [plot.vectorfield()]
 #' @export
 fit_vf_2d <- function(d, x, y,
-											x_start, x_end, x_by, y_start, y_end, y_by,
-											vector_position = "middle", na_action = "omit_data_points",
-											method = "VFC", ...) {
+											x_start = min(d[,x], na.rm = TRUE) - 0.1*(max(d[,x], na.rm = TRUE)-min(d[,x], na.rm = TRUE)),
+											x_end = max(d[,x], na.rm = TRUE) + 0.1*(max(d[,x], na.rm = TRUE)-min(d[,x], na.rm = TRUE)),
+											x_by = 0.05*(max(d[,x], na.rm = TRUE)-min(d[,x], na.rm = TRUE)),
+											y_start = min(d[,y], na.rm = TRUE) - 0.1*(max(d[,y], na.rm = TRUE)-min(d[,y], na.rm = TRUE)),
+											y_end = max(d[,y], na.rm = TRUE) + 0.1*(max(d[,y], na.rm = TRUE)-min(d[,y], na.rm = TRUE)),
+											y_by = 0.05*(max(d[,x], na.rm = TRUE)-min(d[,x], na.rm = TRUE)),
+											vector_position = "start",
+											na_action = "omit_data_points",
+											method = c("VFC", "MVKE"), ...) {
   # extract useful data for construction
   if (is.data.frame(d)) {
-    dv <- d[, c(x, y)] %>% as.matrix()
+    d_raw <- d[, c(x, y)] %>% as.matrix()
   } else if (is.matrix(d)) {
-    dv <- d[, c(x, y)]
+    d_raw <- d[, c(x, y)]
   } else {
     rlang::abort("`d` must be a data frame or a matrix.")
   }
@@ -31,10 +37,12 @@ fit_vf_2d <- function(d, x, y,
 		rlang::abort('`na_action` must be either "omit_data_points" or "omit_vectors".')
 	}
 
+	dv <- normalize_vecs(d_raw)
+
 	if(any(is.na(dv))){
 		{
 			if(na_action == "omit_data_points") {
-				dv <- stats::na.omit(dv)
+				dv <- attr(dv, "x_noNA")
 				rlang::inform("NA(s) found in the data. Those data points were omitted.")
 			}
 		}
@@ -52,33 +60,39 @@ fit_vf_2d <- function(d, x, y,
     rlang::abort('`vector_position` must be one of "start", "middle", or "end".')
   }
 
-  original_vectors <- cbind(x_mat, v_mat) %>%
+  original_vectors_normalized <- cbind(x_mat, v_mat) %>%
   	`colnames<-`(c("x", "y", "vx", "vy"))
 
-  if(any(is.na(original_vectors))){
+  if(any(is.na(original_vectors_normalized))){
   	{
   		if(na_action == "omit_vectors") {
-  			original_vectors <- stats::na.omit(original_vectors)
+  			original_vectors_normalized <- stats::na.omit(original_vectors_normalized)
   			rlang::inform("NA(s) found in the data. Those vectors were omitted.")
   		}
   	}
   }
 
+  original_vectors <- original_vectors_normalized
+  original_vectors[,"x"] <- original_vectors[,"x"] %>% denormalize_x(dv)
+  original_vectors[,"y"] <- original_vectors[,"y"] %>% denormalize_y(dv)
+  original_vectors[,"vx"] <- original_vectors[,"vx"] %>% scale_up(dv)
+  original_vectors[,"vy"] <- original_vectors[,"vy"] %>% scale_up(dv)
+
   VFCresult <- MVKEresult <- NULL
-  method <- toupper(method)
+  method <- toupper(method[1])
   if(method == "VFC"){
-  	VFCresult <- SparseVFC::SparseVFC(original_vectors[,1:2], original_vectors[,3:4], ...)
+  	VFCresult <- SparseVFC::SparseVFC(original_vectors_normalized[,1:2], original_vectors_normalized[,3:4], ...)
   } else if(method == "MVKE") {
-  	MVKEresult <- MVKE(original_vectors[,1:2], ...)
+  	MVKEresult <- MVKE(original_vectors_normalized[,1:2], ...)
   }
 
   vec <- expand.grid(x = seq(x_start, x_end, x_by), y = seq(y_start, y_end, y_by))
   vec <- vec %>% dplyr::rowwise()
 
   if(method == "VFC") {
-  	vec <- vec %>% dplyr::mutate(v = list(stats::predict(VFCresult, c(x, y))))
+  	vec <- vec %>% dplyr::mutate(v = list(stats::predict(VFCresult, c(x, y)%>% normalize_v(dv)) %>% scale_up(dv)))
   } else if(method == "MVKE") {
-  	vec <- vec %>% dplyr::mutate(v = list(MVKEresult(c(x, y))$mu))
+  	vec <- vec %>% dplyr::mutate(v = list(MVKEresult(c(x, y)%>% normalize_v(dv))$mu%>% scale_up(dv)))
   }
 
   vec <- vec %>%
@@ -93,8 +107,10 @@ fit_vf_2d <- function(d, x, y,
   	vec_grid = vec,
   	VFCresult = VFCresult,
   	MVKEresult = MVKEresult,
-  	data = dv,
+  	data = d_raw,
+  	data_normalized = dv,
   	original_vectors = original_vectors,
+  	original_vectors_normalized = original_vectors_normalized,
   	x_start = x_start,
   	x = x,
   	y = y,
@@ -116,6 +132,7 @@ fit_vf_2d <- function(d, x, y,
 #' This is done by a decomposition method described in the reference, using a modified version of [waydown::approxPot2D()].
 #'
 #' @param vf A `vectorfield` object estimated by [fit_vf_2d()]. The other parameters will be inherited from the `vectorfield` object if not otherwise specified.
+#' @param x_sparse,y_sparse A number. For calculating the non-gradient part of the vector field, how much should the sample points be sparser than the sample points for the potential landscape?
 #' @param ... Other parameters passed to [approxPot2D()].
 #'
 #' @export
@@ -125,14 +142,14 @@ fit_vf_2d <- function(d, x, y,
 #' @export
 #' @seealso [plot.2d_vf_landscape()]
 fit_vfld_2d <- function(vf,
-												x_start = vf$x_start, x_end = vf$x_end, x_by = vf$x_by,
-												y_start = vf$y_start, y_end = vf$y_end, y_by = vf$y_by, x_sparse = 1, y_sparse = 1, ...){
+												x_start = vf$x_start, x_end = vf$x_end, x_by = vf$x_by/5,
+												y_start = vf$y_start, y_end = vf$y_end, y_by = vf$y_by/5, x_sparse = 5, y_sparse = 5, ...){
 	if(!inherits(vf, "vectorfield")) rlang::abort("`vf` must be a `vectorfield` object.")
 	xs <- seq(x_start, x_end, x_by)
 	ys <- seq(y_start, y_end, y_by)
 
-	if(vf$method == "VFC") f <- function(x) stats::predict(vf$VFCresult, x)
-	else if(vf$method == "MVKE") f <- function(x) vf$MVKEresult(x)$mu
+	if(vf$method == "VFC") f <- function(x) stats::predict(vf$VFCresult, x %>% normalize_v(vf$data_normalized)) %>% scale_up(vf$data_normalized)
+	else if(vf$method == "MVKE") f <- function(x) vf$MVKEresult(x %>% normalize_v(vf$data_normalized))$mu %>% scale_up(vf$data_normalized)
 	wdresult <- approxPot2D(f, xs = xs, ys = ys, ...)
 	wdresult$xs <- xs
 	wdresult$ys <- ys
